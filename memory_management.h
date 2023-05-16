a696edb715497928a187ebb560f686574ee31077
Burlen Loring <bloring@lbl.gov>
2023-05-16 16:39:44 -0700

    default to pinned memory on the host when cuda is available

--- memory_management.h
+++ memory_management.h
@@ -1,10 +1,16 @@
 #ifndef memory_management_h
 #define memory_management_h
 
+#include "hamr_config.h"
 #include "hamr_buffer.h"
 #include "hamr_buffer_util.h"
 
+#if defined(HAMR_ENABLE_CUDA)
+inline constexpr hamr::buffer_allocator cpu_alloc() { return hamr::buffer_allocator::cuda_host; }
+#else
 inline constexpr hamr::buffer_allocator cpu_alloc() { return hamr::buffer_allocator::malloc; }
+#endif
+
 inline constexpr hamr::buffer_allocator gpu_alloc() { return hamr::buffer_allocator::openmp; }
 
 #if defined(NEWTONPP_ENABLE_OMP)
